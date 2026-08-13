0bb32257009cc6d9f96b1838c550d273cf9e6a63
daferali20 <31751340+daferali20@users.noreply.github.com>
2026-08-14 01:33:34 +0300

    Update App.tsx

--- frontend/web_dashboard/src/App.tsx
+++ frontend/web_dashboard/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import LandingPage from './pages/LandingPage';
-import SubscriptionPage from './pages/Subscription'; // تم التعديل للاستيراد المباشر من المجلد
+import SubscriptionPage from './pages/Subscription';
 import TermsAndConditions from './components/TermsAndConditions';
 import './App.css';
 
