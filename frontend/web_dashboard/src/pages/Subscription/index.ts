7a8b5eea2fc6a0b1332d64ac1085dfe2a560322e
daferali20 <31751340+daferali20@users.noreply.github.com>
2026-08-14 01:30:27 +0300

    Update index.ts

--- frontend/web_dashboard/src/pages/Subscription/index.ts
+++ frontend/web_dashboard/src/pages/Subscription/index.ts
@@ -1,8 +1,2 @@
-// تصدير الصفحة الرئيسية
 export { default as SubscriptionPage } from './SubscriptionPage';
-
-// تصدير كافة أنواع TypeScript الخاصة بالاشتراكات
-export * from './types';
-
-// إعادة تصدير المكونات الفرعية التابعة للاشتراكات (اختياري)
-export * from './components';
+export { default } from './SubscriptionPage';
